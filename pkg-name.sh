d4e7273d67deea0df479448ccd297f4425f39fe5
ReaperMantis <anthony.f.chiaramonte@gmail.com>
2025-07-10 20:56:32 -0400

    Update pkg-name.sh

--- pkg-name.sh
+++ pkg-name.sh
@@ -10,17 +10,9 @@
  	grep Depends control | sed 's/,//g' | sed 's/Depends://' | sed 's/kubuntu-settings-desktop//' | sed 's/plymouth-theme-kubuntu-logo//' | sed 's/plymouth-theme-kubuntu-text//' | sed 's/ubuntu-release-upgrader-qt//' >> aa
 	grep Recommends control | sed 's/,//g' | sed 's/Recommends://' | sed 's/kubuntu-wallpapers//' | sed 's/kubuntu-web-shortcuts//' | sed 's/libreoffice-calc//' | sed 's/libreoffice-impress//' | sed 's/libreoffice-kf5//' | sed 's/libreoffice-plasma//' | sed 's/libreoffice-style-breeze//' | sed 's/libreoffice-writer//' | sed 's/firefox//' >> aa
 	# echo -n 'apt-get -y install ' >> aa
-	echo -n ' gdm3 linux-firmware snapd cloud-initramfs-growroot ' >> aa
-	echo -n ' oem-config-gtk ubiquity-frontend-gtk ubiquity-slideshow-ubuntu yaru-theme-unity yaru-theme-icon yaru-theme-gtk aptdaemon ' >> aa
+	echo -n ' linux-firmware cloud-initramfs-growroot ' >> aa
+	echo -n ' oem-config-gtk yaru-theme-unity yaru-theme-icon yaru-theme-gtk aptdaemon ' >> aa
 	echo -n 'grub-efi-arm64 initramfs-tools' >> aa
 	cd /
 	/bin/bash /aaa/DEBIAN/aa
  
-
-apt install -y \
-	kde-plasma-desktop \
-	linux-firmware \
-	cloud-initramfs-growroot \
-	initramfs-tools \
- 	oem-config-prepare \
-	grub-efi-arm64
